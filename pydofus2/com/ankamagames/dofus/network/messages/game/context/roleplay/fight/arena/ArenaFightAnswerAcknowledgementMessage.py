f8ca44fb1f2d87606b2e671060ca2caa3aa78c9c
Hexalgo <hexalgo@gmail.com>
2024-03-16 09:36:17 +0100

    maj 16/03/2024

---
+++ pydofus2/com/ankamagames/dofus/network/messages/game/context/roleplay/fight/arena/ArenaFightAnswerAcknowledgementMessage.py
@@ -0,0 +1,9 @@
+from pydofus2.com.ankamagames.jerakine.network.NetworkMessage import NetworkMessage
+
+class ArenaFightAnswerAcknowledgementMessage(NetworkMessage):
+    acknowledged: bool
+    def init(self, acknowledged_: bool):
+        self.acknowledged = acknowledged_
+        
+        super().__init__()
+    
